829209ed2f771503c39a0e6cae5c2ae15e89ea36
Ashok <125790623+AshokKanuganti@users.noreply.github.com>
2023-02-26 21:18:16 +0530

    Update streamlit_app.py
    if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruitevice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
+try:
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json())#just writes the date to the screen
@@ -38,12 +38,17 @@ streamlit.header("Fruityvice Fruit Advice!")
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show the tablar manner date into the screen
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+in not fruit_choice
+streamlit.error("Please select a fruit  to get information")
+else:
+#streamlit.write('The user entered ', fruit_choice)
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 #stopping stream lite
 streamlit.stop()
 
