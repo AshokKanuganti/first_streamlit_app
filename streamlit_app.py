c38cb528e697fe6ef5e9fa180687cc92dd9a02e8
Ashok <125790623+AshokKanuganti@users.noreply.github.com>
2023-02-26 21:19:06 +0530

    Update streamlit_app.py
    : miss

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ try:
 # show the tablar manner date into the screen
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
-in not fruit_choice
+in not fruit_choice:
 streamlit.error("Please select a fruit  to get information")
 else:
 #streamlit.write('The user entered ', fruit_choice)
